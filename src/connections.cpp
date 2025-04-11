a4226f2e09d0f02ef53530bd4046637b5aabc129
Gustav Rietschel <gustav.rietschel@gmail.com>
2025-04-11 08:52:17 +0200

    error handling and enabling nvs

--- src/connections.cpp
+++ src/connections.cpp
@@ -1,23 +1,32 @@
 #include "esp_netif.h"
 #include "esp_wifi.h"
+#include "nvs_flash.h"
 #include "connections.h"
+
 
 void ESP_NOW::wifiInit() 
 {
-    esp_netif_init();
-    esp_event_loop_create_default();
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    esp_wifi_set_storage(WIFI_STORAGE_RAM);
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_start();
-    esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE));
 }
 
 
 void ESP_NOW::start(onDataSend sendCb, onDataReceive receiveCb) {
    wifiInit();
-   esp_now_init();
+   ESP_ERROR_CHECK(esp_now_init());
    esp_now_register_send_cb(sendCb);
    esp_now_register_recv_cb(receiveCb);
 };
@@ -33,5 +42,6 @@ void ESP_NOW::addNewESP(uint8_t macAddr[6])
 
 void ESP_NOW::sendData(uint8_t macAddr[6], const void* data, size_t len)
 {
-    esp_now_send(macAddr, reinterpret_cast<const uint8_t*>(data), len);
+    ESP_ERROR_CHECK(esp_now_send(macAddr, reinterpret_cast<const uint8_t*>(data), len));
+    vTaskDelay(1000);
 }
